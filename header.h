5e1bb67db798f3ab22cf531f9078e945f4d858c3
Harish <harichilka@gmail.com>
2022-06-01 06:23:53 -0700

    header for main module

---
+++ header.h
@@ -0,0 +1,51 @@
+/*
+1. Name of the module                           : Main module
+2. Date of creation                             : 01/06/2022
+3. Author of Module                             : Dileep reddy
+4. Description of module                        : The purpose of this module is to creating the online response from for users based on user choice of training , giving  feedback  to attended trainings and voting 
+5. Different functions supported in this module : main();
+
+						   training();
+
+						   voting();
+
+					           registration();
+
+						   voting_request();
+
+						   voting_response_module();
+
+						   feedback_module();
+*/
+
+
+
+
+
+
+
+/**************HEADER FILE*****************************/
+#include<stdio.h>
+#include<stdlib.h>
+
+/******************FUNCTION PROTOTYPES*****************/
+void main();
+
+void training();
+
+void voting();
+
+void registration();
+
+void voting_request();
+
+void voting_response();
+
+void feedback_module();
+
+
+
+
+
+
+
